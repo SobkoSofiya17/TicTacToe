5e103fc75819df5999f91d20a2136452e6711205
Sobko Sofiya <s.sobko@neurone.ru>
2022-12-07 12:44:12 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Haha />
+</template>
+
+<script>
+import Haha from './components/Haha.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Haha
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
